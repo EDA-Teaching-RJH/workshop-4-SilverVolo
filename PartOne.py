ddb305dc91afde69d9d4d8041d331ce21bcfdd72
SilverVolo <do350@kent.ac.uk>
2024-10-16 10:24:49 +0000

    done

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,10 @@
+camel = input("Input camelsss: ")
+index = 0
+for i in camel:
+    if i == i.upper() and index != 0:
+        camel = camel[:index] + "_" + camel[index:]
+        index += 1
+        
+    index += 1
 
+print(camel)
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,19 @@
+current_Money = 0
 
+while current_Money < 75:
+    
+    money = int(input("""Input Coins
+                  50p
+                  20p
+                  10p
+                  5p
+                  : """))
+    current_Money += money
+    moneyLeft = 75 - current_Money
+    print(str(moneyLeft)+"p left to give.")
+    
+    if current_Money >= 75:
+        change = current_Money - 75
+
+print("Your change is: "+str(change)+"p")
+    
